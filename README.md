b022b9c6a8e1ada6bfb838e89ce500b9a30ee888
hoangnv <hoangthao.dev@gmail.com>
2024-02-24 20:33:58 +0700

    section 4

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # shopDev
 base a shop
-
-section 2
-section 3
--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,8 +1,10 @@
 'use strict'
 const mongoose = require("mongoose")
-const connectString = 'mongodb://localhost:27017/shopDEV'
+const { db: { host, port, name } } = require('../configs/config.mongodb')
+const connectString = `mongodb://${host}:${port}/${name}`
 const { countConnect } = require('../helpers/check.connect')
 
+console.log(`connectionString::${connectString}`)
 class Database {
     constructor() {
         this.connect()
