a23e3875de24f60ebff1b2a935802a15be4f9cbd
hoangnv <hoangthao.dev@gmail.com>
2024-02-25 21:46:14 +0700

    section 5

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ const app = express()
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
 //init db
 require('./dbs/init.mongodb')
@@ -16,13 +20,7 @@ require('./dbs/init.mongodb')
 // checkOverload()
 
 //init routes
-app.get('/', (req, res, next) => {
-    const strCompress = 'Hello fantipjs'
-    return res.status(200).json({
-        messenge: 'Welcome shopDev',
-        metadata: strCompress.repeat(100000)
-    })
-})
+app.use('/', require('./routes'))
 
 //handling error
 
